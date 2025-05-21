5ec1d4510ecef329abc3bb14c17025a358cb4eb4
herman2lv <herman2lv@gmail.com>
2025-05-21 22:22:22 +0300

    Add discovery

--- service-bravo/src/main/java/com/hrm/cloud/bravo/BravoApplication.java
+++ service-bravo/src/main/java/com/hrm/cloud/bravo/BravoApplication.java
@@ -2,8 +2,10 @@ package com.hrm.cloud.bravo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class BravoApplication {
 
     public static void main(String[] args) {
